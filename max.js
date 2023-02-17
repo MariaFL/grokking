0646a3bf443b75445575f5954c84658e0f46000d
Maria Federova <fedmaria2@gmail.com>
2023-02-17 07:53:52 +0300

    edited function max by answers in the book

--- max.js
+++ max.js
@@ -1,10 +1,15 @@
 function max(arr) {
     if (arr.length === 0) {
-        return 0
+        return null;
+    }
+    if (arr.length === 1) {
+        return arr[0];
     }
     let next = max(arr.slice(1));
     return arr[0] > next ? arr[0] : next;
 }
 
-
+console.log(max([]));
+console.log(max([-1, -3]));
+console.log(max([1, 9]));
 console.log(max([4, 1, 7, 2]))
